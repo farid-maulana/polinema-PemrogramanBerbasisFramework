4bcd503f37597be024f078e741d5e86b63998f26
farid-maulana <faridmaulana1921@gmail.com>
2022-03-08 10:15:54 +0700

    Week 3 - React Component

---
+++ Modul 3/web-reactku/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Test from './Test';
+import reportWebVitals from './reportWebVitals';
+
+const HelloComponent = () => {
+  return HelloComponent;
+}
+
+class StateFullComponent extends React.Component {
+  render(){
+    return <p>StateFullComponent</p>
+  }
+}
+
+ReactDOM.render(<Test/>, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
