43c431f65361ee30ad4a7e199484e9737043831c
Farid Maulana <52988155+farid-maulana@users.noreply.github.com>
2022-04-17 16:42:28 +0700

    Week 7 - Redux

---
+++ Modul 6/redux/src/App.js
@@ -0,0 +1,13 @@
+import CreateTodo from './containers/CreateTodo';
+import Table from './containers/Table';
+
+function App() {
+  return (
+    <div className='container pt-5'>
+      <CreateTodo />
+      <Table />
+    </div>
+  );
+}
+
+export default App;
