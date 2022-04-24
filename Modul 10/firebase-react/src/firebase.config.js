78012a8c9089ab847cfcf9f3070182820b8b9ae8
Farid Maulana <52988155+farid-maulana@users.noreply.github.com>
2022-04-24 11:44:02 +0700

    Week 10 - Firebase React

---
+++ Modul 10/firebase-react/src/firebase.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  apiKey: "AIzaSyBrJUdgPavb0Ty0VgGJXg7zUAB_An3E5wU",
+  authDomain: "fir-react-f63c2.firebaseapp.com",
+  projectId: "fir-react-f63c2",
+  storageBucket: "fir-react-f63c2.appspot.com",
+  messagingSenderId: "549252777129",
+  appId: "1:549252777129:web:a5a94376a26d2aa29cd5b8",
+  measurementId: "G-Q3850NQZ9Q"
+};
---
+++ Modul 10/firebase-react/src/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import routes from './routes';
+import firebase from 'firebase';
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig)
+
+export const AuthContext = React.createContext(null)
+
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false)
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      Is logged in? {JSON.stringify(isLoggedIn)}
+      <div className='App'>
+        <Router>
+          <Header />
+
+          <Switch>
+            {
+              routes.map(route => (
+                <Route 
+                  key={route.path}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main} />
+              )) 
+            }
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+reportWebVitals();
